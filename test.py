33400b75b8a5a13d48c2d790924c69514a85434a
PKpattranit999719 <103948605+PKpattranit999719@users.noreply.github.com>
2025-02-07 17:13:26 +0700

    ลบส่วนที่ไม่ได้ใช้ออก

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import streamlit as st
 from Map_project.function_routes import (
     DisplayMap, convert_locations, display_route_explanation, explain_route_with_llm, 
     get_route_data, get_route_path_from_id, process_places_of_interest_routes, 
-    search_places_of_interest, recommend_places,extract_and_return_data_from_places
+    search_places_of_interest, recommend_places
 )
 
 # หัวข้อหน้าเว็บ
@@ -17,25 +17,10 @@ radius = 1  # รัศมีการค้นหาสถานที่ 5 ก
 keyword = process_places_of_interest_routes(places_interest)
 st.write(f"Extracted Keyword: {keyword}")  # เช็คค่า keyword ที่ได้
 
-# 1. ส่งค่าพิกัดเริ่มต้นและปลายทางเพื่อหาข้อมูลเส้นทาง
+# ส่งค่าพิกัดเริ่มต้นและปลายทางเพื่อหาข้อมูลเส้นทาง
 flon, flat, tlon, tlat = convert_locations(user_location, user_destination)
 
-# # 2. ใช้ข้อมูลเส้นทางในการหาสถานที่ที่เกี่ยวข้อง
-# status_placeholder = st.empty()  # สร้างที่ว่างสำหรับข้อความสถานะ
-# with st.spinner("⏳ กำลังโหลดข้อมูล..."):
-#     route_data = get_route_data(flon, flat, tlon, tlat)
-#     places_with_coordinates = get_route_path_from_id(route_data)  # โหลดค่าจริงๆ
-# print(places_with_coordinates)
-
-# # # 3. ค้นหาสถานที่ตาม keyword จากเส้นทาง
-# path = get_route_path_from_id(route_data['data'][0]['id'])
-# places_of_interest = search_places_of_interest(path, keyword, radius)
-# print(places_of_interest)
-# # if places_of_interest:
-# #     st.write(f"Places of Interest: {places_of_interest}")  # ตรวจสอบสถานที่ที่ได้
-# # else:
-# #     st.error("ไม่พบสถานที่ที่น่าสนใจตามคำค้นหาของคุณ")
-
+# ใช้ข้อมูลเส้นทางในการหาสถานที่ที่เกี่ยวข้อง
 status_placeholder = st.empty()  # สร้างที่ว่างสำหรับข้อความสถานะ
 with st.spinner("⏳ กำลังโหลดข้อมูล..."):
     # 1. ดึงข้อมูลเส้นทางจากจุดเริ่มต้นและจุดหมายปลายทาง
@@ -47,10 +32,9 @@ with st.spinner("⏳ กำลังโหลดข้อมูล..."):
         route_path_list = get_route_path_from_id(route_id)
         # print("Route Path List:", route_path_list)
 
-        # 3. ค้นหาสถานที่ที่น่าสนใจจากเส้นทาง
+        # ค้นหาสถานที่ที่น่าสนใจจากเส้นทาง
         places_of_interest = search_places_of_interest(route_path_list, keyword, radius)
 status_placeholder.text("การค้นหาสถานที่เสร็จสิ้นแล้ว")
-
 
 route_markers = [
     { "lon": flon, "lat": flat, "title": "จุดเริ่มต้น"  },
